31babd4429a4b39062ac0c8722caf695c5f2bc61
Bartosz Szmidel 3I <bartekszm@onet.pl>
2024-11-28 11:22:57 +0100

    dalej nierodka

--- style.js
+++ style.js
@@ -21,10 +21,39 @@ Struktura DOM przypomina drzewo, na szczycie którego jest obiekt: document
     problemem jest to, że ma mniej elementów niż tablica
 
 2. DWIE METODY ZROBIENIA TABLICY Z POBRANYCH ELEMENTÓW, KTÓRE SĄ OBIEKTEM TABLICOPODOBNYM: */
+// Metoda A
 //document.images jest obiektem tablicopodobnym, który poniżej zmieniam w tablicę
 // na obiekcie Array wykonuję metodę from(), która informuje zrób tablicę z tego co jest w nawiasach
 const imgs = Array.from(document.images)
+//Metoda B
+// ... operator rest , []
+const imgs1 = [...document.images]
 
+/* 3. DWIE METODY POBIERAJĄCE JEDEN ELEMENT
+document powołuje się do całej strony, ale my chcemy się odwołać do konkretnego elementu
+metoda A: */
+//document.querySelector("li:nth-child(3)");
+
+// metoda B. getElementById() - interesuje mnie juz nie selektor, ale sama nazwa id
+const firstElement = document.getElementById('first')
+//gdybym chciał w JS za pomocą querySelector() pobrać to co mam w nawiasach, nie mógłbym wpisać ('first'), ale musiałbym zapisać ('#first')
+document.querySelector("li:nth-child(6)")
+// Jeśli nie znajdę jakiegoś elementu to dostanę null i nie ma błędu
+
+// 4. METODY POBIERAJĄCE WSZYSTKIE PASUJĄCE ELEMENTY A NIE TYLKO JEDEN
+// Praca z jednym elementem jest łatwiejsza ponieważ ma pewne metody i właściwości
+// Jak pobierzemy wiele elementów to są one umieszczone w obiekcie tablicopodobnym, który musimy zmienić w tablicę, wtedy jednak będziemy musieli wykonać: 
+// jakąś pętlę
+// albo jakąś metodę 
+// aby dostać się do każdego elementu  
+
+//WNIOSEK inaczej pracuje się kiedy ma się pobrany jeden element tak jak powyżej, a inaczej kiedy ma się pobranych WIELE ELEMENTÓW
+//document.querySelector() - pobiera jeden element
+//document.querySelectorAll() - pobiera wiele elementów
+document.querySelectorAll("ul>li");
+// Pobieram wszystkie li, które są dzieckiem ul
+// Zwrócona zostanie tu NodeLost - czyli lista węzłów, obiekt TABLICOPODOBNY
+// który też ma długość length i metody tablicy, ale mniej niż tablica
 
 
 /* II. ----------- MODYFIKOWANIE ELEMENTÓW NA STRONIE INTERNETOWEJ --------------
